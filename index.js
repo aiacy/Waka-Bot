2b1eb9445a9b1fdd22c49a7d63d1d0d045b8c906
Aliyah Regina <96841812+aiacy@users.noreply.github.com>
2022-03-01 21:32:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]}); 
+const prefix = "!waka";
+
+client.on('messageCreate', msg => {
+  
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+
+   const commandBody = msg.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  
+  if (command === 'leader') {
+    const timeTaken = Date.now() - msg.createdTimestamp;
+    msg.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+  }
+});
+
+client.login(process.env['BOT_TOKEN']);
