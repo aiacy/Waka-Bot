7ec251965b30914c7e51febbb5180f3d71a6c24c
Aliyah Regina <96841812+aiacy@users.noreply.github.com>
2022-03-02 17:37:11 +0000

    register commands in deploy-command.js file

---
+++ deploy-commands.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('./config.json');
+
+const commands = [
+  new SlashCommandBuilder().setName('join').setDescription('Replies with join list'),
+  new SlashCommandBuilder().setName('leader').setDescription('Replies with Leaderboard info'),
+  new SlashCommandBuilder().setName('list-cc').setDescription('Replies with the list of possible country codes'),
+  new SlashCommandBuilder().setName('list-lang').setDescription('Replies with the list of possible languages'),
+  new SlashCommandBuilder().setName('leader-cc').setDescription('Replies with leader based on country'),
+  new SlashCommandBuilder().setName('leader-lang').setDescription('Replies with leader based on language'),
+]
+  .map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken('OTQ3OTc1MzE4NDA3MzYwNjEz.Yh1E0A.sZDs_SynVSli_I-qOA1UxqWkqAU');
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const prefix = "!waka-";
 
 const joinURL = 'https://wakatime.com/leaders/sec/e6fdf763-88e0-4923-9df1-d51c216acc09/join/wamfwwypqg';
 
+const userId = 'fe84537b-4baf-440a-815d-4afcd47068a0'
+const leaderboardId = 'e6fdf763-88e0-4923-9df1-d51c216acc09';
+
 client.on('messageCreate', msg => {
 
   async function getLeader() {
@@ -13,18 +16,42 @@ client.on('messageCreate', msg => {
   if (msg.author.bot) return;
   if (!msg.content.startsWith(prefix)) return;
 
-   const commandBody = msg.content.slice(prefix.length);
+  const commandBody = msg.content.slice(prefix.length);
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
 
   
-  if (command === 'leader') {
+  if (command === 'join'){
+    msg.reply(`So you want to join the AIACY WakaTime Leaderboard? Click to join ${joinURL}`);
+  }
+    //TODO: Confirm WakaTime user based off email
+  else if (command === 'confirm'){
+    msg.reply('You can add your name to the list so when the leaderboard posts, you\'ll be able to see your name (by default WakaTime sets it to Annyonomus User)')    
+  }
+    //TODO: Create Top 10 Leaderboard Worldwide
+  else if (command === 'leader') {
     const timeTaken = Date.now() - msg.createdTimestamp;
     msg.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
   }
-  else if (command === 'join'){
-    msg.reply(`So you want to join the AIACY WakaTime Leaderboard? Click to join ${joinURL}`);
+    //TODO: List out all the cc in a seperate []
+  else if (command === 'list-cc') {
+    msg.reply('This is the Country Code List!')
+  }
+    //TODO: List out all the lang in a seperate []
+  else if (command === 'list-lang'){
+    msg.reply('This is the Languages List')
+  }
+    //TODO: List out the leaders based off of country
+  else if (command === 'leader-cc') {
+    msg.reply('This is the leader of the board in the desired country')
+  }
+    //TODO: List out the leaders based off of language
+  else if (command === 'leader-lang'){
+    msg.reply('This is the leader of the board in the desired language')
   }
 });
 
+
 client.login(process.env['BOT_TOKEN']);
+
+//TODO: create database to store WakaTime user ids to be able to read from them and promote rank 
